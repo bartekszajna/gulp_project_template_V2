8c1ab8e0c08095d40e15cf2fe3427c5716ff612b
Bartek <bartek.szajna@wp.pl>
2020-05-25 23:07:55 +0200

    Ready to use project template
    I like things to be neat and clean as far as I can so I kept track of
    changes during configuration to not forget it in the future. This commit
    is intended to be ultimate version I'm going to use in my work flow

--- gulpfile.js
+++ gulpfile.js
@@ -5,55 +5,37 @@ const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const browserSync = require('browser-sync').create();
 const webpack = require('webpack-stream');
-//const webpack = require('webpack');
 const imagemin = require('gulp-imagemin');
 const color = require('gulp-color');
-//const minify = require('gulp-minify');
-//const terser = require('gulp-terser');
-//const babel = require('gulp-babel-minify');
-//const babel = require('gulp-babel');
-//const uglify = require('gulp-uglify');
-//const browserify = require('browserify');
 const inject = require('gulp-inject');
 const del = require('del');
 
-/* dependencies commented out above were used during configuration. Somehow I couldn't 
-make neither terser, babel, browserify nor uglify to work so I finally went for webpack and left
-them just to keep track of packages I tried*/
-
-// compile scss into css
 function styles() {
-  return (
-    gulp
-      .src('./src/styles/**/*.scss')
-      .pipe(
-        sass({
-          outputStyle: 'compressed', // options - extended, nested, compressed, compact
-        })
-      )
-      .on('error', sass.logError)
-      // add autoprefixers for some multi-browser problematic styles
-      .pipe(autoprefixer({ overrideBrowserslist: ['last 2 versions', '>1%'] }))
-      .pipe(rename({ suffix: '.min' }))
-      .pipe(cleanCSS())
-      .pipe(gulp.dest('./dist/styles'))
-      .pipe(browserSync.stream())
-  ); //  stream changes to all browsers
+  return gulp
+    .src('./src/styles/**/*.scss')
+    .pipe(
+      sass({
+        outputStyle: 'compressed',
+      })
+    )
+    .on('error', sass.logError)
+    .pipe(autoprefixer({ overrideBrowserslist: ['last 2 versions', '>1%'] }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./dist/styles'))
+    .pipe(browserSync.stream());
 }
 
 function scripts() {
-  del.sync(['./dist/scripts/*.js']); // cleans scripts directory from all old [hash].js files
+  del.sync(['./dist/scripts/*.js']);
   const target = gulp.src('./dist/index.html');
   const scripts = gulp
     .src('./src/scripts/**/*.js')
-    // .pipe(babel()) // the same as higher - previous ideas to bundle code but didn't work for me
-    // .pipe(uglify())
-
-    .pipe(webpack()) // bundles all scripts into one
+    .pipe(webpack())
     .pipe(gulp.dest('./dist/scripts'));
 
-  return target // dynamically injects [hash].js script name to index.html
-    .pipe(inject(scripts, { relative: true })) // solves the problem with relative paths between files
+  return target
+    .pipe(inject(scripts, { relative: true }))
     .pipe(gulp.dest('dist'))
     .pipe(browserSync.stream());
 }
@@ -65,7 +47,6 @@ function images() {
     .pipe(gulp.dest('./dist/images'));
 }
 
-// exporting all functions outside
 exports.styles = styles;
 exports.scripts = scripts;
 exports.images = images;
